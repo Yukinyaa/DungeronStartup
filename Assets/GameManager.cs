486bc23ce08c486dd278b36e103bd53c495cd38f
JeongYeongJin <dudwls12127@gmail.com>
2020-12-09 23:31:05 +0900

    Stage 1, 2, 3 Environments

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Healthbar hp = null;
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Boom" :
+                Destroy(other.gameObject);
+                hp.TakeDamage(10);
+            break;    
+        }
+        if (other.gameObject.GetComponent<Eatable>() == true)
+        {
+            GameObject.Destroy(other.gameObject);
+            EffectManager.instance.DoNom(other.transform.position);
+            hp.health += 20;
+        }
+    }
+}
